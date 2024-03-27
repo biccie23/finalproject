d5e947278255c8e8d595e2fc3a31128538127eea
Leona <leonak34@gmail.com>
2024-03-27 21:13:20 +1100

    updated index,nav and footer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-const name = document.getElementById('name');
+let lastScrollTop = 0;
+let isNavbarVisible = true;
+
+window.addEventListener("scroll", function() {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop; // Declare currentScroll here
+  
+  if (currentScroll > lastScrollTop && isNavbarVisible) {
+    // Downscroll code
+    document.querySelector(".navbar").classList.remove("show");
+    isNavbarVisible = false;
+   
+  } else if (currentScroll < lastScrollTop && !isNavbarVisible) {
+    // Upscroll code
+    document.querySelector(".navbar").classList.add("show");
+    isNavbarVisible = true;
+    
+  }
+
+  lastScrollTop = currentScroll; // Move this line inside the event listener function
+}, false);
