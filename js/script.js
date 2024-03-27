d3a22e55b6625afade4572d861c913f240801a41
Leona <leonak34@gmail.com>
2024-03-27 21:03:21 +1100

    updated index tabe

--- js/script.js
+++ js/script.js
@@ -1,10 +1,41 @@
-const navToggle = document.getElementsByClassName('nav-toggle')[0];
-const navLinks = document.getElementsByClassName('nav-link');
+/* NAVBAR TOGGLE FOR WHEN CONVERTS TO BURGER MENU */
 
-navToggle.addEventListener('click', function(){
-    for(var i=0; navLinks.length; i++)
-        navLinks[i].classList.toggle('active');
-    
-    
 
+document.addEventListener('DOMContentLoaded', function() {
+    const navToggle = document.querySelector('.nav-toggle');
+    const navLinks = document.querySelectorAll('.nav-link');
+    const introContainer = document.querySelector('.intro-container');
+
+    navToggle.addEventListener('click', function() {
+        introContainer.classList.toggle('intro-active');
+        
+        for (let i = 0; i < navLinks.length; i++) {
+            navLinks[i].classList.toggle('active');
+        }
+    });
 });
+
+
+/* NAVBAR STICK TO TOP OF PAGE WHEN SCROLLING UPWARDS */
+
+let lastScrollTop = 0;
+let isNavbarVisible = true;
+
+window.addEventListener("scroll", function() {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+  let navbar = document.querySelector(".navbar");
+
+  if (currentScroll > lastScrollTop && isNavbarVisible) {
+    // Downscroll code
+    navbar.classList.remove("show-nav");
+    isNavbarVisible = false;
+  } else if (currentScroll < lastScrollTop && !isNavbarVisible) {
+    // Upscroll code
+    navbar.classList.add("show-nav");
+    isNavbarVisible = true;
+  }
+
+  lastScrollTop = currentScroll;
+}, false);
+
+
